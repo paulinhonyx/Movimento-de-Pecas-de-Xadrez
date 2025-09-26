556ef40f422cff81664fe53a545580abcf231bb1
paulinhonyx <paulinhonyx@hotmail.com>
2025-09-26 10:53:00 -0300

    commit inicial

---
+++ Xadrez.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bem-vindo ao programa de movimentação de peças de xadrez!\n");
+    // Seu código para movimentação de peças vai aqui
+
+    return 0;
+}
