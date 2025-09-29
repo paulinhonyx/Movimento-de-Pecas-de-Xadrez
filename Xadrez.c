d1bb3986c2ddee9b82a3775a9ac2613331565cfe
paulinhonyx <paulinhonyx@hotmail.com>
2025-09-29 14:53:26 -0300

    Desafio: nível mestre

--- Xadrez.c
+++ Xadrez.c
@@ -1,44 +1,64 @@
 #include <stdio.h>
 
-int main() {
-    // Torre 5 casas para esquerda
-    int i = 0;
-    do
-    {
-        printf("Direita\n");
-        i++;
-    } while (i < 5);
-    
-    // Bispo 5 casas na diagonal para cima esquerda
-    for ( i = 0; i < 5; i++)
-    {
-        printf("Cima Direita\n");
-    }
-    
-    //rainha 8 casas para esquerda
-    i = 0;
-    while (i < 8)
+// Função para mover a torre
+void movimentoTorre(int casas) {
+    if (casas > 0)
     {
         printf("Esquerda\n");
-        i++;
+        movimentoTorre(casas - 1);
     }
+    
+}
+
+// Função para mover o bispo
+void movimentoBispo(int vertical) {
+    if (vertical > 0) {
+        printf("Cima\n");
+        for (int horizontal = 0; horizontal < 1; horizontal++) {
+            printf("Esquerda\n");
+        }
+        movimentoBispo(vertical - 1);
+    }
+}
+
+// Função para mover a rainha
+void movimentoRainha(int casas) {
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        movimentoRainha(casas - 1);
+    }
+    
+}
+
+// Função para mover o cavalo
+void movimentoCavalo(cima, direita) {
+    for (cima = 1; cima <= 2; cima++) {
+        printf("Cima\n");
+        if (cima == 2) {
+            for (direita = 1; direita <= 1; direita++) {
+                printf("Direita\n");
+                break; 
+            }
+        }
+    }
+}
+
+int main() {
+    // Torre 5 casas para esquerda
+    movimentoTorre(5);
+    printf("\n");
+    
+    // Bispo 5 casas na diagonal para cima esquerda
+    movimentoBispo(5);
+    printf("\n");
+    
+    //rainha 8 casas para esquerda
+    movimentoRainha(8);
     printf("\n");
 
     // Movimento do cavalo
-    int baixo = 2;
-    int esquerda = 1;
-    while (esquerda <= 1)
-    {
-        for ( i = 0; i < baixo; i++)
-        {
-            printf("Baixo\n");
-        }
-        printf("Esquerda\n");
-        esquerda++;
-        
-    }
-    
-    
+    movimentoCavalo(2, 1);
     
     return 0;
 }
