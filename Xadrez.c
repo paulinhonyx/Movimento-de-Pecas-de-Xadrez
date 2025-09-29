36fd53732d699947ff7e59c973a89b20d2ef8889
paulinhonyx <paulinhonyx@hotmail.com>
2025-09-29 10:46:47 -0300

    Desafio: nível novato

--- Xadrez.c
+++ Xadrez.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    printf("Bem-vindo ao programa de movimentação de peças de xadrez!\n");
-    // Seu código para movimentação de peças vai aqui
-
+    // Torre 5 casas para direita
+    int i = 0;
+    do
+    {
+        printf("Direita\n");
+        i++;
+    } while (i < 5);
+    printf("\n");
+    
+    // Bispo 5 casas na diagonal para cima direita
+    for ( i = 0; i < 5; i++)
+    {
+        printf("Cima Direita\n");
+    }
+    printf("\n");
+    
+    //rainha 8 casas para esquerda
+    i = 0;
+    while (i < 8)
+    {
+        printf("Esquerda\n");
+        i++;
+    }
+    
     return 0;
 }
